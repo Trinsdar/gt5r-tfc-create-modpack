b0d9e831d6431e6a401b7a6ce3cd270020e8d064
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-12-17 20:12:14 -0500

    fixed startup script

--- kubejs/startup_scripts/new_items.js
+++ kubejs/startup_scripts/new_items.js
@@ -1,6 +1,8 @@
 
 onEvent('item.registry', event =>
+{
     event.create('wrought_iron_chain_mesh').displayName('Wrought Iron Chain Mesh')
+
     event.create('incomplete_brass_mechanisms').displayName('Incomplete Brass Mechanisms')
     event.create('incomplete_circular_saw').displayName('Incomplete Circular Saw')
     event.create('circular_saw').displayName('Circular Saw')
